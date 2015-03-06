588ef1cde444738cfe13823a7ed02cce2896a34c
askkaz <askkazgithub@gmail.com>
2015-03-05 20:13:35 -0500

    initial version

---
+++ js/resumeBuilder.js
@@ -0,0 +1,86 @@
+
+var bio = {
+	"name": "Adam",
+	"role":"SW Dev",
+	"welcomeMessage": "Hello and welcome to my resume.",
+	"contacts": {
+		"mobile":"867-5309",
+		"email":"frog@pond.net",
+		"github": "askkaz",
+		"location": "Washington, DC"
+	},
+	"skills": "programming"
+};
+
+var projects = {
+	"projects" : [
+	{
+		"title": "Quora Typeahead Search",
+		"dates": "February 2015",
+		"description": "Solved programming challenge in C++",
+		"images": "http://upload.wikimedia.org/wikipedia/commons/1/18/Vombatus_ursinus_-Maria_Island_National_Park.jpg"
+	},
+	{
+		"title": "FE P1",
+		"dates": "February 2015",
+		"description": "Converted mock up to HTML/CSS",
+		"images": "http://i.imgur.com/NCjy2M8.png"
+	}
+	]
+};
+
+var work = {
+	"jobs" : [
+	{
+		"employer": "PRKK",
+		"title": "Member Technical Staff",
+		"location": "Arlington, VA",
+		"dates": "August 2014 to Present",
+		"description": "Perform modeling and simulation"
+	},
+	{
+		"employer": "IAI",
+		"title": "Senior Systems Engineer / PM",
+		"location": "Reston, VA",
+		"dates": "November 2011 to August 2014",
+		"description": "Systems engineering support"
+	}
+	]
+};
+
+
+var education = {
+	"schools": [
+	{
+		"name": "MIT",
+		"degree":"BS",
+		"majors": "mechanical engineering",
+		"location":"Cambridge, MA",
+		"dates":"2002-2006",
+		"url":"web.mit.edu"
+	},
+	{
+		"name": "Columbia",
+		"degree":"MS",
+		"majors": "mechanical engineering",
+		"location":"New York, NY",
+		"dates":"2007-2010",
+		"url":"columbia.edu"
+	}
+	],
+	"onlineCourses":[
+	{
+		"title":"Javascript basics",
+		"school":"Udacity",
+		"dates":"March 2015",
+		"url":"Udacity.com"
+	},
+	{
+		"title":"Intro to HTML and CSS",
+		"school":"Udacity",
+		"dates":"February 2015",
+		"url":"Udacity.com"
+	}
+	]
+};
+
